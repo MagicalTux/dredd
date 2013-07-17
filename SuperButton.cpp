03ee3e08382f3a4cb8ae5897fa8076f39c51c663
Mark Karpeles <mark@hell.ne.jp>
2013-07-17 10:02:19 +0900

    implementing SuperButton

--- SuperButton.cpp
+++ SuperButton.cpp
@@ -1,5 +1,13 @@
 #include "SuperButton.hpp"
+#include <QMouseEvent>
 
 SuperButton::SuperButton(QWidget *parent): QPushButton(parent) {
+}
+
+void SuperButton::mouseReleaseEvent(QMouseEvent *event) {
+	if (event->button() == Qt::RightButton) {
+		rightClicked();
+	}
+	QPushButton::mouseReleaseEvent(event);
 }
 
--- SuperButton.hpp
+++ SuperButton.hpp
@@ -1,8 +1,16 @@
 #include <QPushButton>
 
+// This is basically nothing more than a right clickable button
+
 class SuperButton: public QPushButton {
 	Q_OBJECT;
 public:
 	SuperButton(QWidget *parent = 0);
+
+signals:
+	void rightClicked();
+
+public slots:
+	void mouseReleaseEvent(QMouseEvent *event);
 };
 
